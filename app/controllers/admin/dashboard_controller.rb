4e11f065f5677c5b809d7880bb1fde959aeef994
faroukalsajee <faroukalsajee@gmail.com>
2021-04-29 03:12:49 +0000

    feature dashbord counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,10 +1,4 @@
 class Admin::DashboardController < ApplicationController
-
-  http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
-  
   def show
-    @category = Category.all
-    @product = Product.all
   end
-
 end
