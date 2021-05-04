d251a3acd64ef3a9fd2fa401e2aa09ed003c6a84
faroukalsajee <faroukalsajee@gmail.com>
2021-05-04 22:15:27 +0000

    add and pass user tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,31 @@
 class User < ActiveRecord::Base
-
   has_secure_password
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, presence: true
+  validates_length_of :password, :minimum => 5
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password_confirmation, presence: true
 
-  validates_presence_of :first_name, 
-  :last_name,
-  :password,
-  :password_confirmation,
-  :email
-
-  validates :password, length: { minimum: 5}
-  validates :email, uniqueness: {:case_sensitive => false}
+  before_save :clean_email
 
   def self.authenticate_with_credentials(email, password)
-      user = User.find_by(email: email[0].downcase.strip).authenticate(password)
-      if (user)
-          user
-      else 
-          nil
-      end
+    return nil if email == nil
+    new_email =
+        if email.is_a?(Array)
+            email.first
+        else 
+            email
+        end
+    @user = User.find_by(email:new_email.gsub(/\s+/, "").downcase)
+    @user && @user.authenticate(password) ? @user : nil
+  end
+
+  private
+
+  def clean_email
+    self.email.downcase!
+    self.email.strip!
   end
 
 end
